159d148d6f0959495f87ef3c12e045a1528fbab7
River <tziony.i@gmail.com>
2024-04-13 16:50:41 +0300

    kde plots are in diff file and made them easier to understand

---
+++ scripts/kde_plots.py
@@ -0,0 +1,145 @@
+from sklearn.neighbors import KernelDensity
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+
+
+
+def silverman_bandwidth(data):
+    # Calculate standard deviation of the data
+    std_dev = np.std(data)
+    # Number of data points
+    n = len(data)
+    # Calculate bandwidth using Silverman's rule of thumb
+    bandwidth = (4 * std_dev**5 / (3 * n))**(1/5)
+    return bandwidth
+
+
+def get_silverman_bandwitdth_single_feature(df):
+    # assumes the df has only one column
+    return silverman_bandwidth(df.values)
+
+def get_silverman_bandwitdth(df):
+    return [get_silverman_bandwitdth_single_feature(df.iloc[:, i]) for i in range(df.shape[1])]
+
+
+
+def plot_kde(values, feature_name, sampler, fixed= False):
+        bandwidth = silverman_bandwidth(values)
+        kde = KernelDensity(bandwidth=bandwidth, kernel='gaussian')
+        # plot it
+        kde.fit(values[:, None])
+        min = np.min(values)
+        max = np.max(values)
+        x = np.linspace(min, max, 100000)
+        logprob = kde.score_samples(x[:, None])
+        plt.fill_between(x, np.exp(logprob), alpha=0.5)
+        plt.title(f'{feature_name} KDE, sampler: {sampler}')
+        plt.show()
+
+
+
+def plot_comparative_kde(values_original, values_sampled, feature_name, sampler, fixed=False):
+    if not fixed:
+         kde = KernelDensity(bandwidth='silverman', kernel='gaussian')
+    else:
+    # the same but compare to kde of values_original
+        bandwidth = silverman_bandwidth(values_sampled) 
+        kde = KernelDensity(bandwidth=bandwidth, kernel='gaussian')
+    # plot it
+    kde.fit(values_original[:, None])
+    min = np.min(values_original)
+    max = np.max(values_original)
+                                                
+    x = np.linspace(min, max, 100000)
+    logprob = kde.score_samples(x[:, None])
+    plt.fill_between(x, np.exp(logprob), alpha=0.5, label='original')
+    kde.fit(values_sampled[:, None])
+
+    x = np.linspace(min, max, 100000)
+    logprob = kde.score_samples(x[:, None])
+    plt.fill_between(x, np.exp(logprob), alpha=0.5, label='sampled')
+    plt.title(f'{feature_name} KDE, sampler: {sampler}')
+    plt.legend()
+    plt.show()
+
+def plot_head_to_head_kde(values_original, values_sampler_1, values_sampler_2, feature_name, sampler_1_name, sampler_2_name, fixed=False):
+        if not fixed:
+            kde = KernelDensity(bandwidth='silverman', kernel='gaussian')
+        else:
+            bandwidth = silverman_bandwidth(values_sampler_1) 
+            # the same but compare to kde of values_original
+            kde = KernelDensity(bandwidth=bandwidth, kernel='gaussian')
+        # plot it
+
+        kde.fit(values_original[:, None])
+        min = np.min(values_original)
+        max = np.max(values_original)                                         
+        x = np.linspace(min, max, 100000)
+        logprob = kde.score_samples(x[:, None])
+        plt.plot(x, np.exp(logprob), color='black', linestyle='-', linewidth=2, label='original')
+
+        kde.fit(values_sampler_1[:, None])
+        x = np.linspace(min, max, 100000)
+        logprob = kde.score_samples(x[:, None])
+        plt.fill_between(x, np.exp(logprob), alpha=0.5, label=sampler_1_name)
+
+        kde.fit(values_sampler_2[:, None])
+        x = np.linspace(min, max, 100000)
+        logprob = kde.score_samples(x[:, None])
+        plt.fill_between(x, np.exp(logprob), alpha=0.5, label=sampler_2_name)
+
+        plt.title(f'{feature_name} KDE')
+        plt.legend()
+        plt.show()
+
+
+def plot_kde_per_feature_sampled(paths, feature_names, sampler_names):
+    dfs = [pd.read_csv(path, header=None) for path in paths]
+
+    for i in range(dfs[0].shape[1]):
+        for j in range(len(dfs)):
+            df = dfs[j]
+            plot_kde(df.iloc[:, i].values, feature_names[i], sampler_names[j])
+
+
+def plot_comparative_kde_per_feature_sampled(original_path, paths, feature_names, sampler_names, fixed=False):
+    original = pd.read_csv(original_path)
+    dfs = [pd.read_csv(path, header=None) for path in paths]
+
+    for i in range(original.shape[1]):
+        for j in range(len(dfs)):
+            df = dfs[j]
+            plot_comparative_kde(original.iloc[:, i].values, df.iloc[:, i].values, feature_names[i], sampler_names[j], fixed=fixed)
+
+
+def plot_head_to_head_kde_per_feature_sampled(original_path, path_1, path_2, feature_names, sampler_1_name, sampler_2_name, fixed=False):
+    original = pd.read_csv(original_path)
+    df_1 = pd.read_csv(path_1, header=None)
+    df_2 = pd.read_csv(path_2, header=None)
+
+    for i in range(original.shape[1]):
+        plot_head_to_head_kde(original.iloc[:, i].values, df_1.iloc[:, i].values, df_2.iloc[:, i].values, feature_names[i], sampler_1_name, sampler_2_name, fixed=fixed)
+
+
+
+
+def plot_head_to_head_kde_per_feature_sampled_concacted(original_path, path_1, path_2, feature_names, sampler_1_name, sampler_2_name, n_files = None, fixed=False):
+    original = pd.read_csv(original_path)
+    # get all csv files in path_1
+    dfs_1 = []
+    for f in os.listdir(path_1):
+        if f.endswith('.csv') and (n_files is None or len(dfs_1) < n_files):
+            dfs_1.append(pd.read_csv(path_1 + '/' + f, header=None))
+    
+    dfs_2 = []
+    for f in os.listdir(path_2):
+        if f.endswith('.csv') and (n_files is None or len(dfs_2) < n_files):
+            dfs_2.append(pd.read_csv(path_2 + '/' + f, header=None))
+    df_1 = pd.concat(dfs_1, axis=0)
+    df_2 = pd.concat(dfs_2, axis=0)
+
+    for i in range(original.shape[1]):
+        plot_head_to_head_kde(original.iloc[:, i].values, df_1.iloc[:, i].values, df_2.iloc[:, i].values, feature_names[i], sampler_1_name, sampler_2_name, fixed=fixed)
