7cc409c7a6bb709fae35f48a3fd2480771829900
River <tziony.i@gmail.com>
2024-04-20 22:30:14 +0300

    safasfasf

--- full_simulation.py
+++ full_simulation.py
@@ -5,6 +5,6 @@ from scripts.config import get_config
 def main():
     config = get_config()
 
-    do_full_simulation(config.dataset_name, sample_sizes=config.sample_sizes, redundancy=config.redundancy, columns_to_use=config.columns_to_use, weights = None)
+    do_full_simulation(config.dataset_name, sample_sizes=config.sample_sizes, redundancy=config.redundancy, columns_to_use=config.columns_to_use, weights = config.weights)
 if __name__ == "__main__":
     main()
--- scripts/config.py
+++ scripts/config.py
@@ -19,6 +19,25 @@ dataset_names_to_sizes = {
     'Wine Quality': [100, 150, 200, 250,350, 500],
 }
 
+# Uses the top 4 feautres with the highest absolute pearson correlation 
+dataset_to_pearson_features_weight= {
+    'Abalone': [1,1,1,0,0,0,0,1,0,0,1],
+    'Insurance': [1,0,1,1,1,0,0,0,0,1],
+    'Melbourne Housing': [1,0,0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    'Seoul Bike': [1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+    'Sleep Efficiency': [0,0,0,0,0,1,1,1,0,1,0,1],
+    'Wine Quality': [0,0,1,0,0,1,0,0,1,0,0,1,1],
+}
+
+dataset_to_shap_features_weight= {
+    'Abalone': [0,1,0,1,1,0,1,0,0,0,1],
+    'Insurance': [1,0,1,1,1,0,0,0,0,1],
+    'Melbourne Housing': [0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1],
+    'Seoul Bike': [1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    'Sleep Efficiency': [0,0,0,0,0,1,1,1,0,0,1,1],
+    'Wine Quality': [0,0,1,0,0,0,1,1,0,0,0,1,1],
+}
+
 
 
 
@@ -34,10 +53,22 @@ def get_config():
     #parser.add_argument('--sample_sizes', type=int, nargs='+', default=[50, 100, 150, 200, 250, 500], help='Sizes of the samples to take')
 
     parser.add_argument('--columns_to_use', type=str , required=True, help='ALL for all columns, FEATURES for only features', choices=['ALL', 'FEATURES'])
+    # Add weight type : options are pearson, shap
+    
+    parser.add_argument('--weight_type', type=str , required=False, help='Type of weight to use for the features', choices=['pearson', 'shap'], default=None)
 
     args =  parser.parse_args()
 
     args.sample_sizes = dataset_names_to_sizes[args.dataset_name] 
+
+    if args.weight_type == None:
+        args.weights = None
+    elif args.weight_type == 'pearson':
+        args.weights = dataset_to_pearson_features_weight[args.dataset_name]
+    elif args.weight_type == 'shap':
+        args.weights = dataset_to_shap_features_weight[args.dataset_name]
+    else:
+        raise ValueError(f"Invalid weight type: {args.weight_type}")
 
     return args
 
